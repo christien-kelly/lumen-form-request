2309eae65627598dad49efaf8cbb95cf80a6e034
Jonathan Down <jdown@cardataconsultants.com>
2022-05-25 10:03:58 -0400

    bugfixes

--- src/Http/FormRequest.php
+++ src/Http/FormRequest.php
@@ -3,7 +3,7 @@
 namespace Monsterlane\Http;
 
 use Laravel\Lumen\Http\Request;
-use Illuminate\Routing\Redirector;
+use Laravel\Lumen\Http\Redirector;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Validation\ValidationException;
@@ -14,236 +14,236 @@ use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 
 class FormRequest extends Request implements ValidatesWhenResolved
 {
-    use ValidatesWhenResolvedTrait;
+	use ValidatesWhenResolvedTrait;
 
-    /**
-     * The container instance.
-     *
-     * @var \Illuminate\Contracts\Container\Container
-     */
-    protected $container;
+	/**
+	 * The container instance.
+	 *
+	 * @var \Illuminate\Contracts\Container\Container
+	 */
+	protected $container;
 
-    /**
-     * The redirector instance.
-     *
-     * @var \Illuminate\Routing\Redirector
-     */
-    protected $redirector;
+	/**
+	 * The redirector instance.
+	 *
+	 * @var \Laravel\Lumen\Http\Redirector
+	 */
+	protected $redirector;
 
-    /**
-     * The URI to redirect to if validation fails.
-     *
-     * @var string
-     */
-    protected $redirect;
+	/**
+	 * The URI to redirect to if validation fails.
+	 *
+	 * @var string
+	 */
+	protected $redirect;
 
-    /**
-     * The route to redirect to if validation fails.
-     *
-     * @var string
-     */
-    protected $redirectRoute;
+	/**
+	 * The route to redirect to if validation fails.
+	 *
+	 * @var string
+	 */
+	protected $redirectRoute;
 
-    /**
-     * The controller action to redirect to if validation fails.
-     *
-     * @var string
-     */
-    protected $redirectAction;
+	/**
+	 * The controller action to redirect to if validation fails.
+	 *
+	 * @var string
+	 */
+	protected $redirectAction;
 
-    /**
-     * The key to be used for the view error bag.
-     *
-     * @var string
-     */
-    protected $errorBag = 'default';
+	/**
+	 * The key to be used for the view error bag.
+	 *
+	 * @var string
+	 */
+	protected $errorBag = 'default';
 
-    /**
-     * The validator instance.
-     *
-     * @var \Illuminate\Contracts\Validation\Validator
-     */
-    protected $validator;
+	/**
+	 * The validator instance.
+	 *
+	 * @var \Illuminate\Contracts\Validation\Validator
+	 */
+	protected $validator;
 
-    /**
-     * Get the validator instance for the request.
-     *
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function getValidatorInstance()
-    {
-        if ($this->validator) {
-            return $this->validator;
-        }
+	/**
+	 * Get the validator instance for the request.
+	 *
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function getValidatorInstance()
+	{
+		if ($this->validator) {
+			return $this->validator;
+		}
 
-        $factory = $this->container->make(ValidationFactory::class);
+		$factory = $this->container->make(ValidationFactory::class);
 
-        if (method_exists($this, 'validator')) {
-            $validator = $this->container->call([$this, 'validator'], compact('factory'));
-        } else {
-            $validator = $this->createDefaultValidator($factory);
-        }
+		if (method_exists($this, 'validator')) {
+			$validator = $this->container->call([$this, 'validator'], compact('factory'));
+		} else {
+			$validator = $this->createDefaultValidator($factory);
+		}
 
-        if (method_exists($this, 'withValidator')) {
-            $this->withValidator($validator);
-        }
+		if (method_exists($this, 'withValidator')) {
+			$this->withValidator($validator);
+		}
 
-        $this->setValidator($validator);
+		$this->setValidator($validator);
 
-        return $this->validator;
-    }
+		return $this->validator;
+	}
 
-    /**
-     * Create the default validator instance.
-     *
-     * @param  \Illuminate\Contracts\Validation\Factory  $factory
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function createDefaultValidator(ValidationFactory $factory)
-    {
-        return $factory->make(
-            $this->validationData(), $this->container->call([$this, 'rules']),
-            $this->messages(), $this->attributes()
-        );
-    }
+	/**
+	 * Create the default validator instance.
+	 *
+	 * @param  \Illuminate\Contracts\Validation\Factory  $factory
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function createDefaultValidator(ValidationFactory $factory)
+	{
+		return $factory->make(
+			$this->validationData(), $this->container->call([$this, 'rules']),
+			$this->messages(), $this->attributes()
+		);
+	}
 
-    /**
-     * Get data to be validated from the request.
-     *
-     * @return array
-     */
-    protected function validationData()
-    {
-        return $this->all();
-    }
+	/**
+	 * Get data to be validated from the request.
+	 *
+	 * @return array
+	 */
+	protected function validationData()
+	{
+		return $this->all();
+	}
 
-    /**
-     * Handle a failed validation attempt.
-     *
-     * @param  \Illuminate\Contracts\Validation\Validator  $validator
-     * @return void
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    protected function failedValidation(Validator $validator)
-    {
-        throw (new ValidationException($validator))
-                    ->errorBag($this->errorBag)
-                    ->redirectTo($this->getRedirectUrl());
-    }
+	/**
+	 * Handle a failed validation attempt.
+	 *
+	 * @param  \Illuminate\Contracts\Validation\Validator  $validator
+	 * @return void
+	 *
+	 * @throws \Illuminate\Validation\ValidationException
+	 */
+	protected function failedValidation(Validator $validator)
+	{
+		throw (new ValidationException($validator))
+			->errorBag($this->errorBag)
+			->redirectTo($this->getRedirectUrl());
+	}
 
-    /**
-     * Get the URL to redirect to on a validation error.
-     *
-     * @return string
-     */
-    protected function getRedirectUrl()
-    {
-        $url = $this->redirector->getUrlGenerator();
+	/**
+	 * Get the URL to redirect to on a validation error.
+	 *
+	 * @return string
+	 */
+	protected function getRedirectUrl()
+	{
+		$url = $this->redirector->getUrlGenerator();
 
-        if ($this->redirect) {
-            return $url->to($this->redirect);
-        } elseif ($this->redirectRoute) {
-            return $url->route($this->redirectRoute);
-        } elseif ($this->redirectAction) {
-            return $url->action($this->redirectAction);
-        }
+		if ($this->redirect) {
+			return $url->to($this->redirect);
+		} elseif ($this->redirectRoute) {
+			return $url->route($this->redirectRoute);
+		} elseif ($this->redirectAction) {
+			return $url->action($this->redirectAction);
+		}
 
-        return $url->previous();
-    }
+		return $url->previous();
+	}
 
-    /**
-     * Determine if the request passes the authorization check.
-     *
-     * @return bool
-     */
-    protected function passesAuthorization()
-    {
-        if (method_exists($this, 'authorize')) {
-            return $this->container->call([$this, 'authorize']);
-        }
+	/**
+	 * Determine if the request passes the authorization check.
+	 *
+	 * @return bool
+	 */
+	protected function passesAuthorization()
+	{
+		if (method_exists($this, 'authorize')) {
+			return $this->container->call([$this, 'authorize']);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Handle a failed authorization attempt.
-     *
-     * @return void
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     */
-    protected function failedAuthorization()
-    {
-        throw new AuthorizationException('This action is unauthorized.');
-    }
+	/**
+	 * Handle a failed authorization attempt.
+	 *
+	 * @return void
+	 *
+	 * @throws \Illuminate\Auth\Access\AuthorizationException
+	 */
+	protected function failedAuthorization()
+	{
+		throw new AuthorizationException('This action is unauthorized.');
+	}
 
-    /**
-     * Get the validated data from the request.
-     *
-     * @return array
-     */
-    public function validated()
-    {
-        return $this->validator->validated();
-    }
+	/**
+	 * Get the validated data from the request.
+	 *
+	 * @return array
+	 */
+	public function validated()
+	{
+		return $this->validator->validated();
+	}
 
-    /**
-     * Get custom messages for validator errors.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [];
-    }
+	/**
+	 * Get custom messages for validator errors.
+	 *
+	 * @return array
+	 */
+	public function messages()
+	{
+		return [];
+	}
 
-    /**
-     * Get custom attributes for validator errors.
-     *
-     * @return array
-     */
-    public function attributes()
-    {
-        return [];
-    }
+	/**
+	 * Get custom attributes for validator errors.
+	 *
+	 * @return array
+	 */
+	public function attributes()
+	{
+		return [];
+	}
 
-    /**
-     * Set the Validator instance.
-     *
-     * @param  \Illuminate\Contracts\Validation\Validator  $validator
-     * @return $this
-     */
-    public function setValidator(Validator $validator)
-    {
-        $this->validator = $validator;
+	/**
+	 * Set the Validator instance.
+	 *
+	 * @param  \Illuminate\Contracts\Validation\Validator  $validator
+	 * @return $this
+	 */
+	public function setValidator(Validator $validator)
+	{
+		$this->validator = $validator;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Set the Redirector instance.
-     *
-     * @param  \Illuminate\Routing\Redirector  $redirector
-     * @return $this
-     */
-    public function setRedirector(Redirector $redirector)
-    {
-        $this->redirector = $redirector;
+	/**
+	 * Set the Redirector instance.
+	 *
+	 * @param  \Laravel\Lumen\Http\Redirector  $redirector
+	 * @return $this
+	 */
+	public function setRedirector(Redirector $redirector)
+	{
+		$this->redirector = $redirector;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Set the container implementation.
-     *
-     * @param  \Illuminate\Contracts\Container\Container  $container
-     * @return $this
-     */
-    public function setContainer(Container $container)
-    {
-        $this->container = $container;
+	/**
+	 * Set the container implementation.
+	 *
+	 * @param  \Illuminate\Contracts\Container\Container  $container
+	 * @return $this
+	 */
+	public function setContainer(Container $container)
+	{
+		$this->container = $container;
 
-        return $this;
-    }
+		return $this;
+	}
 }
--- src/Providers/FormRequestServiceProvider.php
+++ src/Providers/FormRequestServiceProvider.php
@@ -2,19 +2,39 @@
 
 namespace Monsterlane\Providers;
 
+use Illuminate\Contracts\Validation\ValidatesWhenResolved;
+use Laravel\Lumen\Http\Redirector;
 use Illuminate\Support\ServiceProvider;
+
+use Monsterlane\Http\FormRequest;
 
 class FormRequestServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        $this->app->resolving(FormRequest::class, function ($form, $app) {
-            $form = FormRequest::createFrom($app['request'], $form);
-            $form->setContainer($app);
-        });
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 
-        $this->app->afterResolving(FormRequest::class, function (FormRequest $form) {
-            $form->validate();
-        });
-    }
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->app->afterResolving(ValidatesWhenResolved::class, function ($resolved) {
+			$resolved->validateResolved();
+		});
+
+		$this->app->resolving(FormRequest::class, function ($request, $app) {
+			$request = FormRequest::createFrom($app['request'], $request);
+
+			$request->setContainer($app)->setRedirector($app->make(Redirector::class));
+		});
+	}
 }
